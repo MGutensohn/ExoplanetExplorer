ea2a436c4eea17ce545eedaa37131ee6ec796621
Michael Gutensohn <michael@gutensohn.me>
2019-12-09 19:05:40 -0800

    UI integration.

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject menu;
+
+    private Animator animator;
+
+    public void OpenMenu()
+    {
+        animator = menu.GetComponent<Animator>();
+
+        animator.SetBool("Open", true);
+    }
+
+    public void CloseMenu()
+    {
+        animator = menu.GetComponent<Animator>();
+
+        animator.SetBool("Open", false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
